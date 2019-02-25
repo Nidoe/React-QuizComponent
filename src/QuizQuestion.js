2a19b579d654a278b71dc0186facc283c6a70cd6
Luv Somani <lrsomani@gmail.com>
2019-02-24 18:33:58 -0600

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-
+import QuizQuestionButton from './QuizQuestionButton.js'
 class QuizQuestion extends Component{
   constructor(props){
     super(props)
@@ -7,14 +7,12 @@ class QuizQuestion extends Component{
   render(){
     return(<main>
         <section>
-          <p>
-            {this.props.quiz_question.instruction_text}
-          </p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
               <li>
-              {this.props.quiz_question.answer_options[0]}
+               <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
               </li>
           </ul>
         </section>
